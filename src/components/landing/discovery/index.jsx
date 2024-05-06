881fa9339d39cb214b2d50007b610de3c8fd4e5c
Pedram AGUIARD <aguiardpedram@gmail.com>
2024-05-06 14:59:39 +0000

    ajout d'axios + url api

--- src/components/landing/discovery/index.jsx
+++ src/components/landing/discovery/index.jsx
@@ -1,44 +1,48 @@
-import { Badge } from "primereact/badge";
-import { StyleClass } from "primereact/styleclass";
-import { useRef } from "react";
+import React from "react";
+import { Menubar } from "primereact/menubar";
+import { InputText } from "primereact/inputtext";
+import { Avatar } from "primereact/avatar";
 
-function Discovery() {
-  let toggleMenuPhone = useRef(false);
+export default function TemplateDemo() {
+  const items = [
+    {
+      label: "Home",
+      icon: "pi pi-home",
+    },
+    {
+      label: "Features",
+      icon: "pi pi-star",
+    },
+  ];
+
+  function loadProfileImage() {
+    return '/default_user.jpg'
+  }
+
+  const start = (
+    <img
+      alt="logo"
+      src="https://eu.ftp.opendatasoft.com/paris2024/emblem-white.svg"
+      className="mr-2 border-round-2xl p-2 w-4rem"
+    ></img>
+  );
+  const end = (
+    <div className="flex align-items-center gap-2">
+      <InputText
+        placeholder="Search"
+        type="text"
+        className="w-8rem sm:w-auto"
+      />
+      <Avatar
+        image={loadProfileImage()}
+        shape="circle"
+      />
+    </div>
+  );
 
   return (
-    <div
-      className="surface-overlay px-3 shadow-2 flex justify-content-between relative lg:static w-full"
-      style={{minHeight: 84}}
-    >
-      <div className="flex align-items-center justify-content-center bg-surface">
-        <img src="/jo-24.jpg" alt="Image" height="80" />
-      </div>
-      <StyleClass
-        nodeRef={toggleMenuPhone}
-        selector=".menuMobile"
-        enterClassName="hidden"
-        leaveToClassName="hidden"
-        hideOnOutsideClick
-      >
-        <a className="cursor-pointer block lg:hidden align-self-center text-700 mt-1 p-ripple">
-          <i className="pi pi-bars text-4xl"></i>
-        </a>
-      </StyleClass>
-      <div className="lg:flex hidden absolute lg:static left-0 top-100 z-1 shadow-2 lg:shadow-none lg:pr-5 w-full lg:w-auto bg-surface menuMobile">
-        <ul className="list-none p-0 m-0 flex select-none flex-column lg:flex-row border-top-1 border-gray-800 lg:border-top-none flex align-items-center">
-          <li>
-            <a
-              href="/login"              
-              className="h-6 gap-2 flex px-6 py-3 lg:px-3 no-underline align-items-center text-gray-500 hover:text-gray-600 hover:bg-surface font-medium cursor-pointer transition-all transition-duration-150 hover:shadow-2 border-round-2xl"
-            >
-              <span className="block font-medium">Se connecter</span>
-              <i className="pi pi-sign-in text-base lg:text-2xl mr-2 lg:mr-0"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
+    <div className="w-full">
+      <Menubar model={items} start={start} end={end} className="bg-yellow-200" />
     </div>
   );
 }
-
-export default Discovery;
--- src/services/sites/index.js
+++ src/services/sites/index.js
@@ -1,1 +1,10 @@
-// logique des tri etc + les requêtes http avec fetch
+import axios from "axios"
+
+class ApiService {
+  default_url= "https://data.paris2024.org/explore/dataset/paris-2024-sites-de-competition/api/"
+
+
+  async getAllSports() {
+    return await axios.get(this.default_url +'explore/v2.1/catalog/datasets/paris-2024-sites-de-competition/records?select=sports&limit=-1' ) 
+  }
+}
